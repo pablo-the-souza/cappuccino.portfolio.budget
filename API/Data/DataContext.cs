6672116eb594fc65b8be051594c30a266a06687e
pablo-the-souza <53923086+pablo-the-souza@users.noreply.github.com>
2020-11-09 14:40:39 +1000

    1st Commit
    API: Created DataContext and Migration (SQLite)

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,70 @@
+using API.Entities;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Record> Records { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //seed database with dummy data
+            modelBuilder.Entity<Record>().HasData(
+                new Record()
+                {
+                    Id = 1,
+                    Date = new System.DateTime(),
+                    Name = "Woolies",
+                    Value = 20,
+                    CategoryId = 1,
+                    Type = "Expense"
+                },
+
+                new Record()
+                {
+                    Id = 2,
+                    Date = new System.DateTime(),
+                    Name = "Traslink",
+                    Value = 30,
+                    CategoryId = 2,
+                    Type = "Expense"
+                },
+
+                new Record()
+                {
+                    Id = 3,
+                    Date = new System.DateTime(),
+                    Name = "Cafe 64",
+                    Value = 22,
+                    CategoryId = 3,
+                    Type = "Expense"
+                }
+                );
+
+            modelBuilder.Entity<Category>().HasData(
+            new Category()
+            {
+                Id = 1,
+                Name = "Groceries"
+            },
+
+            new Category()
+            {
+                Id = 2,
+                Name = "Transportation"
+            },
+
+            new Category()
+            {
+                Id = 3,
+                Name = "Eating Out"
+            }
+            );
+        }
+    }
+}
---
+++ API/Entities/Record.cs
@@ -0,0 +1,16 @@
+using System;
+using Core.Entities;
+
+namespace API.Entities
+{
+    public class Record
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Value {get; set; }
+        public DateTime Date {get; set; }
+        public int CategoryId {get; set; }
+        public Category Category { get; set; }
+        public string Type {get; set; }
+    }
+}
